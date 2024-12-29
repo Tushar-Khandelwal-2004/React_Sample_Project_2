ad15786684e42da5aebb72d5df6d7ea6549feb2b
Tushar-Khandelwal-2004 <khandelwal.tushar8527@gmail.com>
2024-12-30 02:16:24 +0530

    Created the Contact_Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css"
+import Contact_Header from "./components/Contact_Header/Contact_Header"
 import Navigation from "./components/Navigation/Navigation"
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       <Navigation/>
+      <Contact_Header/>
     </div>
   )
 }
